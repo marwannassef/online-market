e98a48d974be78da5cbd8d533e893ec59e9a325c
Marwan Nassef <marwan.ali.ashour@gmail.com>
2020-05-14 02:35:48 -0500

    review

--- src/main/java/com/miu/onlinemarket/controller/ProductController.java
+++ src/main/java/com/miu/onlinemarket/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.security.Principal;
+import java.util.Base64;
 
 import javax.imageio.ImageIO;
 import javax.validation.Valid;
@@ -31,43 +32,47 @@ import com.miu.onlinemarket.service.SellerService;
 @RequestMapping("/product")
 public class ProductController {
 
-    @Autowired
-    private ProductService productService;
+	@Autowired
+	private ProductService productService;
 
 	@Autowired
 	private SellerService sellerService;
 
-    @GetMapping("/detail")
-    public String displayProductDetails(@RequestParam("id") long id, Model model) throws ResourceNotFoundException {
-        Product product = productService.findById(id);
-        boolean status = product.isPurchasedStatus();
-        model.addAttribute("status",status);
-         model.addAttribute("product",product);
- 		model.addAttribute("itm", new Product());
-        return "productDetails";
-    }
+	@GetMapping("/detail")
+	public String displayProductDetails(@RequestParam("id") long id, Model model) throws ResourceNotFoundException {
+		Product product = productService.findById(id);
+		boolean status = product.isPurchasedStatus();
+		model.addAttribute("status", status);
+		model.addAttribute("product", product);
+		product.getReviews().forEach(review -> {
+	    	if (review.getBuyer().getPhoto() != null && review.getBuyer().getPhoto().length != 0)
+	    		review.getBuyer().setPhotoBase64(Base64.getEncoder().encodeToString(review.getBuyer().getPhoto()));
+		});
+		model.addAttribute("reviews", product.getReviews());
+		model.addAttribute("itm", new Product());
+		return "productDetails";
+	}
 
-    @GetMapping("/addProduct")
-    public String addProduct(Model model, Principal principal) throws ResourceNotFoundException {
-        model.addAttribute("product", new Product());
-        Seller seller = sellerService.findSeller(principal.getName());
-        boolean approved = seller.getApproved();
-        model.addAttribute("approved",approved);
-        return "addProduct";
-    }
+	@GetMapping("/addProduct")
+	public String addProduct(Model model, Principal principal) throws ResourceNotFoundException {
+		model.addAttribute("product", new Product());
+		Seller seller = sellerService.findSeller(principal.getName());
+		boolean approved = seller.getApproved();
+		model.addAttribute("approved", approved);
+		return "addProduct";
+	}
 
+	@RequestMapping(value = "/addProductProcess", method = RequestMethod.POST)
+	public String addProduct(@Valid @ModelAttribute("product") Product product, BindingResult bindingResult,
+			Principal principal, Model model) throws ResourceNotFoundException, IOException {
+		if (bindingResult.hasErrors()) {
+			Seller seller = sellerService.findSeller(principal.getName());
+			boolean approved = seller.getApproved();
+			model.addAttribute("approved", approved);
+			System.out.println(bindingResult);
+			return "addProduct";
+		}
 
-    @RequestMapping(value = "/addProductProcess", method = RequestMethod.POST)
-    public String addProduct(@Valid @ModelAttribute("product") Product product, BindingResult bindingResult,
-    		Principal principal,Model model) throws ResourceNotFoundException, IOException{
-        if(bindingResult.hasErrors()) {
-            Seller seller = sellerService.findSeller(principal.getName());
-            boolean approved = seller.getApproved();
-            model.addAttribute("approved",approved);
-            System.out.println(bindingResult);
-            return "addProduct";
-        }
-        
 		MultipartFile image = product.getImage();
 		if (image != null && !image.isEmpty()) {
 			try {
@@ -87,38 +92,38 @@ public class ProductController {
 			baos.close();
 		}
 
-        Seller seller = sellerService.findSeller(principal.getName());
-        product.setSeller(seller);
-        productService.save(product);
+		Seller seller = sellerService.findSeller(principal.getName());
+		product.setSeller(seller);
+		productService.save(product);
 
-        return "redirect:/home";
-    }
+		return "redirect:/home";
+	}
 
-    @GetMapping("/removeProduct")
-    public String removeProduct(@RequestParam("id") Long id) throws ResourceNotFoundException {
+	@GetMapping("/removeProduct")
+	public String removeProduct(@RequestParam("id") Long id) throws ResourceNotFoundException {
 
-        Product product =productService.findById(id);
-        productService.delete(product);
+		Product product = productService.findById(id);
+		productService.delete(product);
 
-        return "redirect:/home";
-    }
+		return "redirect:/home";
+	}
 
-    @GetMapping("/updateProduct")
-    public String updateProduct(@RequestParam("id") Long id,Model model) throws ResourceNotFoundException {
+	@GetMapping("/updateProduct")
+	public String updateProduct(@RequestParam("id") Long id, Model model) throws ResourceNotFoundException {
 
-        Product product =productService.findById(id);
-        model.addAttribute("updateProduct",product);
+		Product product = productService.findById(id);
+		model.addAttribute("updateProduct", product);
 
-        return "update-product";
-    }
+		return "update-product";
+	}
 
-    @RequestMapping(value = "/updateProductProcess", method = RequestMethod.POST)
-    public String updateProductProcess(@Valid @ModelAttribute("product") Product product, BindingResult bindingResult,@RequestParam("id") Long id,
-    		Principal principal,Model model) throws ResourceNotFoundException{
-        if(bindingResult.hasErrors()) {
-            model.addAttribute("updateProduct",product);
-            return "update-product";
-        }
+	@RequestMapping(value = "/updateProductProcess", method = RequestMethod.POST)
+	public String updateProductProcess(@Valid @ModelAttribute("product") Product product, BindingResult bindingResult,
+			@RequestParam("id") Long id, Principal principal, Model model) throws ResourceNotFoundException {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("updateProduct", product);
+			return "update-product";
+		}
 		MultipartFile image = product.getImage();
 		if (image != null && !image.isEmpty()) {
 			try {
@@ -127,10 +132,10 @@ public class ProductController {
 				e.printStackTrace();
 			}
 		}
-        Seller seller = sellerService.findSeller(principal.getName());
-        product.setSeller(seller);
-        productService.update(product,id);
-        return "redirect:/home";
-    }
+		Seller seller = sellerService.findSeller(principal.getName());
+		product.setSeller(seller);
+		productService.update(product, id);
+		return "redirect:/home";
+	}
 
 }
